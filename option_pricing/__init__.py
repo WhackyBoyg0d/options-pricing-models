d86bee0a986426aa7f999e08c563fc261cb58ede
Aditya <aditya@Adityas-MacBook-Air.local>
2024-10-06 23:28:52 +0800

    refactoring

--- option_pricing/__init__.py
+++
@@ -1,1 +0,0 @@
-from ..BinomialTreeModel import BinomialTreeModel
--- panel.py
+++ panel.py
@@ -12,7 +12,7 @@ from ticker import Ticker
 
 class OPTION_PRICING_MODEL(en.Enum):
     BINOMIAL_TREE = "Binomial Tree Model"
-    MONTE_CARLO = "Monte Carlo Simulation"
+    # MONTE_CARLO = "Monte Carlo Simulation"
     BLACK_SCHOLES = "Black-Scholes Model"
 
 pn.extension('tabulator')
@@ -32,88 +32,77 @@ def get_options_pricing(pricing_model):
     risk_free_rate = pn.widgets.FloatSlider(name="Risk Free Rate (%)", start=0, end=100, value= 20)
     sigma = pn.widgets.FloatSlider(name="Sigma (%)", start=0, end=100, value= 30)
     exercise_date = pn.widgets.DatePicker(name="Exercise Date", value=dt.datetime.now())
+    button = pn.widgets.Button(name="Calculate Option Price")
 
     if pricing_model == OPTION_PRICING_MODEL.BINOMIAL_TREE.value:
         number_of_time_steps = pn.widgets.IntSlider(name="Number of Time Steps", start=5000, end=100000, value=15000)
-        button = pn.widgets.Button(name="Calculate Option Price")
+        
 
-        def calculate_option_price(event):
-            """Calculate the option price using Binomial Tree Model."""
-            if event:
-                print(ticker.value)
-                historical_data = get_historical_data(ticker.value)
-                spot_price = Ticker.get_last_price(historical_data, "Close")
-                binomial_tree_model = BinomialTreeModel(spot_price, strike_price.value, risk_free_rate.value, sigma.value, (exercise_date.value - dt.date.today()).days, number_of_time_steps.value)
-                tabulator = pn.widgets.Tabulator(historical_data, pagination="remote", page_size=10, disabled = True, width=800)
-                plot = historical_data.hvplot.line(x='Date', y='Close', title=f'{ticker.value} Stock Price', width=800, height=400)
-                call_option_price = binomial_tree_model.calculate_option_price("Call")
-                put_option_price = binomial_tree_model.calculate_option_price("Put")
-                return pn.Column(tabulator, plot, pn.pane.Markdown(f"Call Option Price: {call_option_price}"), pn.pane.Markdown(f"Put Option Price: {put_option_price}"))
-            else:
-                return pn.pane.Markdown("Click the button to calculate the option price.")
-        
-        gspec = pn.GridSpec(sizing_mode='stretch_both', max_height=500)
+    def calculate_option_price(event):
+        """Calculate the option price using Binomial Tree Model."""
+        if event:
+            print(ticker.value)
+            historical_data = get_historical_data(ticker.value)
+            spot_price = Ticker.get_last_price(historical_data, "Close")
+            if pricing_model == OPTION_PRICING_MODEL.BINOMIAL_TREE.value:
+                options_pricing = BinomialTreeModel(spot_price, strike_price.value, risk_free_rate.value, sigma.value, (exercise_date.value - dt.date.today()).days, number_of_time_steps.value)
+            elif pricing_model == OPTION_PRICING_MODEL.BLACK_SCHOLES.value:
+                options_pricing = BlackScholesModel(spot_price, strike_price.value, (exercise_date.value - dt.date.today()).days, risk_free_rate.value, sigma.value)
+            title_tabularor = pn.pane.Markdown(f"# Stock Price Data for {ticker.value}")
+            title_plot = pn.pane.Markdown(f"# Stock Price Chart for {ticker.value}")
+            tabulator = pn.widgets.Tabulator(historical_data, pagination="remote", page_size=10, disabled = True, width=800)
+            tabulator = pn.Column(title_tabularor, tabulator)
+            plot = historical_data.hvplot.line(x='Date', y='Close', title=f'{ticker.value} Stock Price', width=800, height=400)
+            plot = pn.Column(title_plot, plot)
+            call_option_price = options_pricing.calculate_option_price("Call")
+            put_option_price = options_pricing.calculate_option_price("Put")
+            markdown_content = f"""
+            <h1>Option Pricing</h1>
+            <div style="border: 2px solid #4CAF50; padding: 10px; border-radius: 5px;">
+            <strong>Call Option Price:</strong> {call_option_price}<br>
+            <strong>Put Option Price:</strong> {put_option_price}
+            </div>
+            """
+            return pn.Column(pn.Row(tabulator, plot), markdown_content)
+        else:
+            return pn.pane.Markdown("Click the button to calculate the option price.")
+    
+    gspec = pn.GridSpec(sizing_mode='stretch_both', max_height=500)
+    if pricing_model == OPTION_PRICING_MODEL.BINOMIAL_TREE.value:
         gspec[0, 0] = pn.Column(ticker, strike_price, risk_free_rate, sigma, exercise_date, number_of_time_steps, button)
-        gspec[0, 1:3] = pn.bind(calculate_option_price, button)
-        return gspec
-    
     elif pricing_model == OPTION_PRICING_MODEL.BLACK_SCHOLES.value:
-        button = pn.widgets.Button(name="Calculate Option Price")
-        
-        def calculate_option_price2(event):
-            """Calculate the option price using Black-Scholes Model."""
-            if event:
-                historical_data = get_historical_data(ticker.value)
-                spot_price = Ticker.get_last_price(historical_data, "Close")
-                black_scholes_model = BlackScholesModel(spot_price, strike_price.value, (exercise_date.value - dt.date.today()).days, risk_free_rate.value, sigma.value)
-                tabulator = pn.widgets.Tabulator(historical_data, pagination="remote", page_size=10, disabled = True, width=800)
-                plot = historical_data.hvplot.line(x='Date', y='Close', title=f'{ticker.value} Stock Price', width=800, height=400)
-                call_option_price = black_scholes_model.calculate_option_price("Call")
-                put_option_price = black_scholes_model.calculate_option_price("Put")
-                markdown_content = f"""
-                <div style="border: 2px solid #4CAF50; padding: 10px; border-radius: 5px;">
-                <strong>Call Option Price:</strong> {call_option_price}<br>
-                <strong>Put Option Price:</strong> {put_option_price}
-                </div>
-                """
-                return pn.Column(pn.Row(tabulator, plot), markdown_content)
-            else:
-                return pn.pane.Markdown("Click the button to calculate the option price.")
-        
-        gspec = pn.GridSpec(sizing_mode='stretch_both', max_height=500)
         gspec[0, 0] = pn.Column(ticker, strike_price, risk_free_rate, sigma, exercise_date, button)
-        gspec[0, 1:3] = pn.bind(calculate_option_price2, button)
-        return gspec
-    
-    elif pricing_model == OPTION_PRICING_MODEL.MONTE_CARLO.value:
-        number_of_simulations = pn.widgets.IntSlider(name="Number of Simulations", start=5000, end=100000, value=15000)
-        button = pn.widgets.Button(name="Calculate Option Price")
+    gspec[0, 1:3] = pn.bind(calculate_option_price, button)
+    return gspec
+    # elif pricing_model == OPTION_PRICING_MODEL.MONTE_CARLO.value:
+    #     number_of_simulations = pn.widgets.IntSlider(name="Number of Simulations", start=5000, end=100000, value=15000)
+    #     button = pn.widgets.Button(name="Calculate Option Price")
         
-        def calculate_option_price3(event):
-            """Calculate the option price using Monte Carlo Simulation."""
-            if event:
-                historical_data = get_historical_data(ticker.value)
-                spot_price = Ticker.get_last_price(historical_data, "Close")
-                monte_carlo_simulation = MonteCarloSimulation(spot_price, strike_price.value, (exercise_date.value - dt.date.today()).days, risk_free_rate.value, sigma.value, number_of_simulations.value)
-                monte_carlo_simulation.simulate_prices()
-                tabulator = pn.widgets.Tabulator(historical_data, pagination="remote", page_size=10, disabled = True, width=800)
-                plot = monte_carlo_simulation.plot_simulation()
-                call_option_price = monte_carlo_simulation.calculate_option_price("Call")
-                put_option_price = monte_carlo_simulation.calculate_option_price("Put")
-                markdown_content = f"""
-                <div style="border: 2px solid #4CAF50; padding: 10px; border-radius: 5px; background-color: #f9f9f9;">
-                <strong>Call Option Price:</strong> {call_option_price}<br>
-                <strong>Put Option Price:</strong> {put_option_price}
-                </div>
-                """
-                return pn.Column(tabulator, plot, pn.pane.Markdown(f"Call Option Price: {call_option_price}"), pn.pane.Markdown(f"Put Option Price: {put_option_price}"))
-            else:
-                return pn.pane.Markdown("Click the button to calculate the option price.")
+    #     def calculate_option_price3(event):
+    #         """Calculate the option price using Monte Carlo Simulation."""
+    #         if event:
+    #             historical_data = get_historical_data(ticker.value)
+    #             spot_price = Ticker.get_last_price(historical_data, "Close")
+    #             monte_carlo_simulation = MonteCarloSimulation(spot_price, strike_price.value, (exercise_date.value - dt.date.today()).days, risk_free_rate.value, sigma.value, number_of_simulations.value)
+    #             monte_carlo_simulation.simulate_prices()
+    #             tabulator = pn.widgets.Tabulator(historical_data, pagination="remote", page_size=10, disabled = True, width=800)
+    #             plot = monte_carlo_simulation.plot_simulation()
+    #             call_option_price = monte_carlo_simulation.calculate_option_price("Call")
+    #             put_option_price = monte_carlo_simulation.calculate_option_price("Put")
+    #             markdown_content = f"""
+    #             <div style="border: 2px solid #4CAF50; padding: 10px; border-radius: 5px; background-color: #f9f9f9;">
+    #             <strong>Call Option Price:</strong> {call_option_price}<br>
+    #             <strong>Put Option Price:</strong> {put_option_price}
+    #             </div>
+    #             """
+    #             return pn.Column(tabulator, plot, pn.pane.Markdown(f"Call Option Price: {call_option_price}"), pn.pane.Markdown(f"Put Option Price: {put_option_price}"))
+    #         else:
+    #             return pn.pane.Markdown("Click the button to calculate the option price.")
         
-        gspec = pn.GridSpec(sizing_mode='stretch_both', max_height=500)
-        gspec[0, 0] = pn.Column(ticker, strike_price, risk_free_rate, sigma, exercise_date, number_of_simulations, button)
-        gspec[0, 1:3] = pn.bind(calculate_option_price3, button)
-        return gspec
+    #     gspec = pn.GridSpec(sizing_mode='stretch_both', max_height=500)
+    #     gspec[0, 0] = pn.Column(ticker, strike_price, risk_free_rate, sigma, exercise_date, number_of_simulations, button)
+    #     gspec[0, 1:3] = pn.bind(calculate_option_price3, button)
+    #     return gspec
         
 
 
